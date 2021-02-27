32c9c45b2a8fedb3548858267a9e692aaa3d248b
LucasPanao <panaotrabalho@gmail.com>
2021-02-27 15:56:40 -0300

    Criando pagina de nova lista, já conectada com API

---
+++ frontend/src/app/pages/new-list/new-list.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { TaskService } from 'src/app/task.service';
+
+@Component({
+  selector: 'app-new-list',
+  templateUrl: './new-list.component.html',
+  styleUrls: ['./new-list.component.scss']
+})
+export class NewListComponent implements OnInit {
+
+  constructor( private TaskService: TaskService ) { }
+
+  ngOnInit(): void {
+  }
+
+  createList(title: string){
+    this.TaskService.createList(title).subscribe((response: any) => {
+      console.log(response)
+    })
+  }
+
+}
