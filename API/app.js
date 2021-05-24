2f4a32925e0595323eb1c4e9ebfb4155a6cb97fc
LucasPanao <panaotrabalho@gmail.com>
2021-05-23 21:59:22 -0300

    Criando chamadas de API de LOGIN

--- API/app.js
+++ API/app.js
@@ -6,7 +6,9 @@ const {mongoose} = require('./db/mongoose')
 const bodyParser = require('body-parser')
 
 // Load mongoose models
-const {List, Task} = require('./db/models')
+const {List, Task, User} = require('./db/models')
+
+//Middleware
 
 //load middleware
 app.use(bodyParser.json());
@@ -18,6 +20,54 @@ app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
   });
+
+// verify refresh token
+
+let verifySession = ((req,res,next) => {
+     // grab the refresh token
+     let refreshToken = req.header('x-refresh-token');
+
+     // grab the _id
+     let _id = req.header('_id');
+ 
+     User.findByIdAndToken(_id, refreshToken).then((user) => {
+         if (!user) {
+             // user couldn't be found
+             return Promise.reject({
+                 'error': 'Usuário não encontrado'
+             });
+         }
+
+         // user found
+
+        req.user_id = user._id;
+        req.userObject = user;
+        req.refreshToken = refreshToken;
+
+        let isSessionValid = false;
+
+        user.sessions.forEach((session) => {
+            if (session.token === refreshToken) {
+                // check if the session has expired
+                if (User.hasRefreshTokenExpired(session.expiresAt) === false) {
+                    // refresh token has not expired
+                    isSessionValid = true;
+                }
+            }
+        });
+
+        if (isSessionValid) {
+            next();
+        } else {
+            // the session is not valid
+            return Promise.reject({
+                'error': 'Refresh token expirado ou invalido'
+            })
+        }
+    }).catch((e) => {
+        res.status(401).send(e);
+    })
+})
 
 /* ROUTE HANDLERS */
 
@@ -126,6 +176,78 @@ app.delete('/lists/:listId/tasks/:taskId', (req,res) => {
         res.send(removedTaskDoc)
     });
 });
+
+// USER ROUTES
+
+/**
+ * POST /users
+ * Purpose: Sign up
+ */
+ app.post('/users', (req, res) => {
+    // User sign up
+
+    let body = req.body;
+    let newUser = new User(body);
+
+    newUser.save().then(() => {
+        return newUser.createSession();
+    }).then((refreshToken) => {
+        // Sucess
+        return newUser.generateAccessAuthToken().then((accessToken) => {
+            // token genereted
+            return { accessToken, refreshToken }
+        });
+    }).then((authTokens) => {
+        // auth Token
+        res
+            .header('x-refresh-token', authTokens.refreshToken)
+            .header('x-access-token', authTokens.accessToken)
+            .send(newUser);
+    }).catch((e) => {
+        res.status(400).send(e);
+    })
+})
+
+
+/**
+ * POST /users/login
+ * Purpose: Login
+ */
+app.post('/users/login', (req, res) => {
+    let email = req.body.email;
+    let password = req.body.password;
+
+    User.findByCredentials(email, password).then((user) => {
+        return user.createSession().then((refreshToken) => {
+            // Sucess
+            return user.generateAccessAuthToken().then((accessToken) => {
+                // token genereted
+                return { accessToken, refreshToken }
+            });
+        }).then((authTokens) => {
+            // auth token
+            res
+                .header('x-refresh-token', authTokens.refreshToken)
+                .header('x-access-token', authTokens.accessToken)
+                .send(user);
+        })
+    }).catch((e) => {
+        res.status(400).send(e);
+    });
+})
+
+/**
+ * GET /users/me/access-token
+ * Purpose: generates and returns an access token
+ */
+ app.get('/users/me/access-token', verifySession, (req, res) => {
+    req.userObject.generateAccessAuthToken().then((accessToken) => {
+        res.header('x-access-token', accessToken).send({ accessToken });
+    }).catch((e) => {
+        res.status(400).send(e);
+    });
+})
+
 
 app.listen(3000, () => {
     console.log("Servidor na porta 3000")
--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { Routes, RouterModule } from '@angular/router';
 import { NewListComponent } from './pages/new-list/new-list.component';
 import { NewTaskComponent } from './pages/new-task/new-task.component';
 import { TaskViewComponent } from './pages/task-view/task-view.component';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
 
 const routes: Routes = [
   {path: '', redirectTo: 'lists', pathMatch: 'full'},
   {path: 'new-list', component: NewListComponent},
+  {path: 'login', component: LoginPageComponent},
   {path: 'lists', component: TaskViewComponent},
   {path: 'lists/:listId', component: TaskViewComponent},
   {path: 'lists/:listId/new-task', component: NewTaskComponent}
